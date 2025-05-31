3f3ad854d3b36d6a6b91777ce1386e378f962b00
Vien Dinh <maxvien@gmail.com>
2025-05-31 09:47:22 +0000

    eslint: config

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,15 @@
 // @ts-check
 import { defineConfig } from "esmate/eslint";
 
-export default defineConfig({
-  type: "app",
-  react: true,
-  ignores: ["src/utilities/storefront/zeus"],
-});
+export default defineConfig(
+  {
+    type: "app",
+    react: true,
+    ignores: ["src/utilities/storefront/zeus", "src/utilities/env.ts"],
+  },
+  {
+    rules: {
+      "react-refresh/only-export-components": "off",
+    },
+  },
+);
