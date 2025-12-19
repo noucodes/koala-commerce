a4e04381c85256512f8391558bb2d2c8d8dece01
Vien Dinh <maxvien@gmail.com>
2025-12-19 20:25:51 +0700

    update metadata for product page

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,5 +1,11 @@
+import { Metadata } from "next";
 import { ProductList } from "./product-list";
 import { getProductList } from "./utils";
+
+export const metadata: Metadata = {
+  title: "Products",
+  description: "All products on the store",
+};
 
 export default async function Page() {
   const data = await getProductList();
